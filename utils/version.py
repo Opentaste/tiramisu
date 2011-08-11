9c1f541933b188c2692d646f723ff8410c5c2780
Gianluca Bargelli <g.bargelli@gmail.com>
2011-08-12 00:55:52 +0200

    Modified the intro example to a more suiting one

--- utils/version.py
+++ utils/version.py
@@ -8,8 +8,8 @@ markdown_intro = """
 Tiramisu is a tiny, modular and fast JavaScript framework 
 which takes inspiration from the famous [jQuery][1] syntax:
 
-    tiramisu.get('h1 #unique').each(function() {{
-        alert(h1.innerHTML);
+    tiramisu.get('h1').each(function() {{
+        alert(h1.html());
     }});
 
 Here are some highlights:
