cf16cd9a0a1fc43c9fafa526bdb741a8524b9270
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2011-09-23 22:31:54 +0200

    Added minified version for 0.1.3-b4

--- src/tiramisu.js
+++ src/tiramisu.js
@@ -19,7 +19,7 @@
      */
 
     function Tiramisu() {
-        this.version = '0.1.3-b3';
+        this.version = '0.1.3-b4';
         this.d = document;
         this.selector = 'QSA'
         this.requestAnimFrame = (function() {
