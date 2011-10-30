f3e10c062206b8063412274949c2fdc8379611a9
Gianluca Bargelli <g.bargelli@gmail.com>
2011-10-30 12:08:57 +0100

    Closes #19

--- src/tiramisu.js
+++ src/tiramisu.js
@@ -19,7 +19,7 @@
      */
 
     function Tiramisu() {
-        this.version = '0.1.3-b16';
+        this.version = '0.1.4';
         this.d = document;
         this.selector = 'QSA'
         this.requestAnimFrame = (function() {
