45cc107a9734b0db784aac6c2cad6d5136ca4f15
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2011-12-17 15:49:14 +0000

    Added basic documentation for .ready() method

--- src/tiramisu.js
+++ src/tiramisu.js
@@ -712,8 +712,25 @@
                 return this;
             },
             /**
-             * Ready
-             * -----------------------
+             * Ready method
+             * ----------------------
+             *
+             * Make sure that DOM elements exist when it run the events.
+             *
+             * Usage
+             * -----
+             *
+             *     tiramisu.get(*SELECTOR*).ready(*FUNCTION*)
+             *
+             * where *SELECTOR* is a valid CSS selector (containing *one* or *more* elements) 
+             * and *FUNCTION* is the function to execute when the DOM is ready.
+             *
+             * Example #1 
+             * -----------------------------------------
+             *
+             *     t.get(document).ready(function(){
+             *          alert('This will be executed when the dom is ready");
+             *     });
              *
              *
              */
