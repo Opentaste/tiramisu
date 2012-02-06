b6fd6465ce76a03f073e53689a72440e506a7071
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2012-02-06 01:02:26 +0000

    Moved Ready function in tiramisu.selector

--- src/modules/tiramisu.selector.js
+++ src/modules/tiramisu.selector.js
@@ -788,6 +788,38 @@ tiramisu.modules.get = function(selector) {
             return this;
         },
         /**
+         * Ready method
+         * ----------------------
+         *
+         * Make sure that DOM elements exist when it run the events.
+         *
+         * Usage
+         * -----
+         *
+         *     tiramisu.get(*SELECTOR*).ready(*FUNCTION*)
+         *
+         * where *SELECTOR* is a valid CSS selector (containing *one* or *more* elements)
+         * and *FUNCTION* is the function to execute when the DOM is ready.
+         *
+         * Example #1
+         * -----------------------------------------
+         *
+         *     t.get(document).ready(function(){
+         *          alert('This will be executed when the dom is ready");
+         *     });
+         *
+         *
+         */
+        'ready': function(def) {
+            t.d.onreadystatechange = function() {
+                if (t.d.readyState == "complete") {
+                    // Run the callback
+                    def();
+                    return this;
+                }
+            }
+        },
+        /**
          * Index extension method
          * ---------------------------------
          *
