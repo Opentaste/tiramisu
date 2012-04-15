4d333fdd01b2c05176927ccbe796142c4fd2350e
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2012-04-16 00:42:08 +0100

    Initial Json module.

---
+++ src/modules/tiramisu.json.js
@@ -0,0 +1,25 @@
+/** 
+ * Framework Json Module
+ * =====================
+ *
+ * This module is mainly used to 
+ *
+ * Usage
+ * -----
+ *
+ *     tiramisu.json(my_json_text, reviver);
+ *
+ * .....
+ *
+ *
+ * Example #1 (...)
+ * -----------------------------
+ *
+ *     tiramisu.json();
+ *
+ *
+ * @api public
+ */
+tiramisu.modules.json = function(my_json_text, reviver) {
+    return '';
+};
--- src/tiramisu.core.js
+++ src/tiramisu.core.js
@@ -19,7 +19,7 @@
      */
     function Tiramisu() {
         
-        this.version = '0.1.7-b2';
+        this.version = '0.1.7-b3';
         this.d = document;
         this.modules = Tiramisu.prototype;
                 
